6b7f26daf5a1a8dc20db5b2b51bd79eaba726ffa
montoyaguzman <montoyaguzman7@gmail.com>
2022-10-20 12:04:39 -0500

    fix: se suben archivos pendientes

--- 1-node-npm/1.1-modulos/es6-import-export-type-module/index.js
+++ 1-node-npm/1.1-modulos/es6-import-export-type-module/index.js
@@ -1,20 +1,20 @@
-import { sum, substract } from './modules/sum-substract.js';
-import * as Operator from './modules/multipli-divide.js';
+import { sum, substract } from "./modules/sum-substract.js";
+import * as Operator from "./modules/multipli-divide.js";
 
 function main() {
-    const num1 = 20;
-    const num2 = 5;
-    const sumResult = sum(num1, num2);
-    console.log(sumResult);
+  const num1 = 20;
+  const num2 = 5;
+  const sumResult = sum(num1, num2);
+  console.log(sumResult);
 
-    const substractResult = substract(num1, num2);
-    console.log(substractResult);
+  const substractResult = substract(num1, num2);
+  console.log(substractResult);
 
-    const multiplyResult = Operator.multiply(num1, num2);
-    console.log(multiplyResult);
+  const multiplyResult = Operator.multiply(num1, num2);
+  console.log(multiplyResult);
 
-    const divideResult = Operator.divide(num1, num2);
-    console.log(divideResult);
+  const divideResult = Operator.divide(num1, num2);
+  console.log(divideResult);
 }
 
 main();
