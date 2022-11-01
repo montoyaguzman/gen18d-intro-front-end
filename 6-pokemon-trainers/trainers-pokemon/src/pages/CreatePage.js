afe5790f9271ba8e2199ea8e1e9d9864555ac4e8
montoyaguzman <montoyaguzman7@gmail.com>
2022-10-31 20:03:14 -0600

    feat: avance de manejo de formularios

--- 6-pokemon-trainers/trainers-pokemon/src/pages/CreatePage.js
+++ 6-pokemon-trainers/trainers-pokemon/src/pages/CreatePage.js
@@ -1,35 +1,87 @@
+import React from 'react';
+
 function CreatePage() {
 
+    const [ trainer, setTrainer ] = React.useState({
+        trainerName: '',
+        rankTrainer: 9999,
+        region: '',
+        // ligas: [],
+        // pokemons: [],
+    });
+
     const handleInput = (event) => {
-        console.log(event.target.id);
-        console.log(event.target.value);
+        // console.log(event);
+        // console.log(event.target); // componente html
+        // console.log(event.target.id); // id componente
+        // console.log(event.target.value); // value que envia el componente
+        const id = event.target.id;
+        const value = event.target.value;
+
+        let newTrainer = { ...trainer };
+
+        // if (id === 'trainerName') {
+        //     // newTrainer.name = value
+        //     newTrainer['name'] = value
+        // }
+        // if (id === 'rankTrainer') {
+        //     // newTrainer.rank = value
+        //     newTrainer['rank'] = value
+        // }
+        newTrainer[id] = value;
+
+        setTrainer(newTrainer);
+        console.log('trainer', trainer);
+
+    }
+
+    const handleSelect = (event) => {
+        const id = event.target.id;
+        const value = event.target.value;
+        console.log(id, value);
+        let newTrainer = { ...trainer };
+        newTrainer[id] = value;
+        console.log('newTrainer', newTrainer)
+        setTrainer(newTrainer);
+        console.log('trainer', trainer);
     }
 
     return(
         <>
             <form>
 
-                <label for="cars">Choose a car:</label>
-                <select name="cars" id="cars">
-                    <option value="volvo">liga 1</option>
-                    <option value="saab">liga 2</option>
+                <span>Entrenador </span>
+                <br/>
+                <label for="fname">Nombre:</label>
+                <input type="text" id="trainerName" name="trainerName" onChange={handleInput}/>
+                <br/>
+                <br/>
+                <label for="rankTrainer">Rango:</label>
+                <input type="text" id="rankTrainer" name="rankTrainer" onChange={handleInput}/>
+
+                <label for="region">Region:</label>
+                <select name="region" id="region" onChange={handleSelect}>
+                    <option value="kanto">kanto</option>
+                    <option value="alola">alola</option>
+                    <option value="johto">johto</option>
                 </select>
 
-                <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
-                <label for="vehicle1"> I have a bike</label><br/>
-                <input type="checkbox" id="vehicle2" name="vehicle2" value="Car"/>
-                <label for="vehicle2"> I have a car</label><br/>
+                {/* <input type="checkbox" id="liga1" name="liga1" value="liga1"/>
+                <label for="liga1">liga 1</label><br/>
+                <input type="checkbox" id="liga2" name="liga2" value="liga2"/>
+                <label for="liga2">liga 2</label><br/>
 
-                <span>Equipo: </span>
-                <label for="fname">First name:</label>
-                <input type="text" id="patitoInput" name="patitoInput" onChange={handleInput}/>
+                <span>Equipo </span>
+                <br/>
+                <label for="pokemonName">Pokemon:</label>
+                <input type="text" id="pokemonName" name="pokemonName" onChange={handleInput}/>
                 <br/>
                 <br/>
-                <label for="lname">Last name:</label>
-                <input type="text" id="lname" name="lname"/>
+                <label for="pokemonLevel">Nivel:</label>
+                <input type="text" id="pokemonLevel" name="pokemonLevel" onChange={handleInput}/>
                 <br/>
                 <br/>
-                <br/>
+                <br/> */}
 
             </form>
         </>
