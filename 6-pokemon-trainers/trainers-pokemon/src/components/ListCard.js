7ffc2ffd3e33a0f456f55e1fe01c84e146eabda5
montoyaguzman <montoyaguzman7@gmail.com>
2022-10-31 18:56:59 -0600

    fix: correcion de class por className

--- 6-pokemon-trainers/trainers-pokemon/src/components/ListCard.js
+++ 6-pokemon-trainers/trainers-pokemon/src/components/ListCard.js
@@ -2,7 +2,7 @@ function ListCard(props) { // props: { children: {lo que venga entrre las tags }
     return(
         <>
             INICIO
-            <div class="cards-container container-80">
+            <div className="cards-container container-80">
                 {props.children}
             </div>
             FIN
