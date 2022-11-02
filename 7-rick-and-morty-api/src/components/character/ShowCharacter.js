8dc9a0fabb1e1cbc3b1188ad9c8e908b5e81ed1d
dannielgloria <danniel.gloria@gmail.com>
2022-11-01 20:01:40 -0600

    Feat: Hooks and components added

---
+++ 7-rick-and-morty-api/src/components/character/ShowCharacter.js
@@ -0,0 +1,12 @@
+import Character from "./Character";
+
+import React from 'react'
+
+const ShowCharacter = ({name}) => {
+    const
+  return (
+    <div>ShowCharacter</div>
+  )
+}
+
+export default ShowCharacter
